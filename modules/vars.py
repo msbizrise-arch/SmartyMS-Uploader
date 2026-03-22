6aa623c305264e604e2fd256740d672b026faca8
msbizrise-arch <msbizrise@gmail.com>
2026-03-22 15:29:40 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,12 +3,12 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22480303"))
-API_HASH = environ.get("API_HASH", "61e5cc94bc5e6318643707054e54caf4")
+API_ID = int(environ.get("API_ID", "38498066"))
+API_HASH = environ.get("API_HASH", "c9696114751feacdeb1b4487f5839a1a")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "8500852075"))
+OWNER = int(environ.get("OWNER", "8703802029"))
 CREDIT = "VIP BROTHERS"
-AUTH_USER = os.environ.get('AUTH_USERS', '8500852075').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8703802029').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
